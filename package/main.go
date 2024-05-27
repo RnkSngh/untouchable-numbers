aa2a836a537d83a2e01c51b457d77cf91ce310e9
Raunak <60561127+RnkSngh@users.noreply.github.com>
2024-05-27 19:04:23 -0400

    get aliquot sequence numbers working (w/o checking for repeats)

--- package/main.go
+++ package/main.go
@@ -2,29 +2,40 @@ package main
 
 import (
 	"fmt"
-	"strconv"
-	"strings"
-	"time"
+	"math/big"
 )
 
-func printNumber(a int){
-	fmt.Println(strings.Repeat(strconv.Itoa(a), a))
-	fmt.Println("printed", a)
+func findAliquotSum(bigA *big.Int)(*big.Int){
+	if bigA.Cmp(big.NewInt(1))==0{
+		return new(big.Int)
+	}
+	sqrt := &big.Int{}
+
+	sqrt.Sqrt(bigA)
+	factorSum := big.NewInt(1)
+	
+	var q, r big.Int 
+	for i:= big.NewInt(2); i.Cmp(sqrt)<=0 ; i.Add(big.NewInt(1), i) {
+		q, r := q.QuoRem(bigA, i, &r)
+		if(r.Cmp(new(big.Int))==0){
+			factorSum.Add(i, factorSum)
+			if(i.Cmp(q)!=0){
+				factorSum.Add(q, factorSum)
+			}
+		}
+	}
+	return factorSum
+	
 }
 
-
-func wait(){
-	time.Sleep(3 * time.Second)
+func findAliquotSequence(a int64){
+	n := 1;
+	for bigA:=big.NewInt(a);  bigA.Cmp(new(big.Int))!=0; bigA = findAliquotSum(bigA){
+		fmt.Printf("The %d aliquot number in this sequence is: %d \n" , n , bigA)
+		n++;
+	}
 }
 
 func main(){
-	c := time.After(3*time.Second) 
-
-	go func() {
-		for i:=0; true; i++{
-			go printNumber(i)
-		}
-	}()
-	
-	<-c
+	findAliquotSequence(138)
 }
